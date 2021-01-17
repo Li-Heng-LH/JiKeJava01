e6d7707111b452e9523c9be21016e866600c48bf
Li Heng <liheng6567@gmail.com>
2021-01-17 23:10:22 +0800

    getParent() and getSuperclass on class loaders

--- week01/src/me/liheng/tryClassLoaders/TryClassLoaders.java
+++ week01/src/me/liheng/tryClassLoaders/TryClassLoaders.java
@@ -2,10 +2,32 @@ package me.liheng.tryClassLoaders;
 
 import com.sun.javafx.util.Logging;
 
+import java.util.ArrayList;
+
 public class TryClassLoaders {
 
     public static void main(String[] args) {
 
-        
+        //AppClassLoader
+        System.out.println("Classloader of this class:" + TryClassLoaders.class.getClassLoader());
+
+        //ExtClassLoader
+        System.out.println("Classloader of Logging:" + Logging.class.getClassLoader());
+
+        //null (bootstrap class loader)
+        System.out.println("Classloader of ArrayList:" + ArrayList.class.getClassLoader());
+
+        //getParent() on AppClassLoader -> ExtClassLoader
+        System.out.println("getParent() on AppClassLoader: " + TryClassLoaders.class.getClassLoader().getParent());
+
+        //getParent() on ExtClassLoader -> null(bootstrap class loader)
+        System.out.println("getParent() on ExtClassLoader: " + Logging.class.getClassLoader().getParent());
+
+        //getSuperclass() on AppClassLoader -> URLClassLoader
+        System.out.println("getSuperclass() on AppClassLoader" + TryClassLoaders.class.getClassLoader().getClass().getSuperclass());
+
+        //getSuperclass() on ExtClassLoader -> URLClassLoader
+        System.out.println("getSuperclass() on ExtClassLoader" + Logging.class.getClassLoader().getClass().getSuperclass());
     }
+
 }
